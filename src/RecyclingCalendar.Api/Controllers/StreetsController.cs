85a29365ed5df7ea8c0600282ef26672ecd4a8f4
Kevin Nalinne <kna@pulsar.be>
2022-01-05 17:33:53 +0100

    add streets controller

--- src/RecyclingCalendar.Api/Controllers/StreetsController.cs
+++ src/RecyclingCalendar.Api/Controllers/StreetsController.cs
@@ -1,17 +1,33 @@
 using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using RecyclingCalendar.Core.DTO;
+using RecyclingCalendar.Core.Interfaces;
 
 namespace RecyclingCalendar.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class StreetsController: ControllerBase
+public class StreetsController : ControllerBase
 {
-    [HttpGet("~/zip-codes/{zipCode}/streets")]
-    [ProducesResponseType(typeof(IList<Street>), (int) HttpStatusCode.OK)]
-    public IActionResult GetStreets(string zipCode)
+    private readonly IStreetService _streetService;
+    private readonly IZipCodeService _zipCodeService;
+
+    public StreetsController(IStreetService streetService, IZipCodeService zipCodeService)
     {
-        return Ok(new List<Street>());
+        _streetService = streetService;
+        _zipCodeService = zipCodeService;
+    }
+
+    [HttpGet("~/zip-codes/{zipCode}/streets")]
+    [ProducesResponseType(typeof(IList<Street>), (int)HttpStatusCode.OK)]
+    public async Task<IActionResult> GetStreets(string zipCode)
+    {
+        var zipCodes = await _zipCodeService.FindByCode(zipCode);
+        return zipCodes.Count switch
+        {
+            > 1 => BadRequest($"zip code {zipCode} is not unique"),
+            0 => BadRequest($"zip code {zipCode} not found"),
+            _ => Ok(await _streetService.FindAll(zipCodes.First().Id))
+        };
     }
 }
